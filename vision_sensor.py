42792491de9daeab87945a9a2391d1c87b89cd27
Stefan Hellwig <stefanhellwig@gmail.com>
2021-09-25 12:19:09 +0200

    Erkenne R G B Werte

--- vision_sensor.py
+++ vision_sensor.py
@@ -1,15 +1,16 @@
 from pylgbst import get_connection_bluegiga
-from pylgbst.hub import MoveHub, VisionSensor
+from pylgbst.hub import MoveHub
+from pylgbst.peripherals import VisionSensor, COLORS
 import time
 
-def callback(clr, distance):
-    print("Color: %s / Distance: %s" % (clr, distance))
+def callback(r, g, b):
+    print("Color: R = %s G = %s B = %s" % (r, g, b))
 
 conn = get_connection_bluegiga(hub_mac="00:16:53:BA:44:01")  # ! don't put this into `try` block
 try:
     hub = MoveHub(conn)
 
-    hub.vision_sensor.subscribe(callback, mode=VisionSensor.COLOR_DISTANCE_FLOAT)
+    hub.vision_sensor.subscribe(callback, mode=VisionSensor.COLOR_RGB)
     time.sleep(60) # play with sensor while it waits   
     hub.vision_sensor.unsubscribe(callback)
 finally:
